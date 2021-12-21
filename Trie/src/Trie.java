c15a693e6de3ec85a59e3a17e1b7fd0c0031867f
Aditya <55052481+aditya019@users.noreply.github.com>
2021-12-22 03:23:53 +0530

    Update Trie.java

--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -37,12 +37,48 @@ public class Trie {
         }
         return true;
     }
+    public void print() {
+        print("", origin);
+    }
+
+    private void print(String s, Node root) {
+        s += root.val;
+        if(root == origin) s = "";
+        if(root.map.containsKey('\0')) System.out.println(s);
+        for(char c : root.map.keySet()) {
+            print(s, root.map.get(c));
+        }
+        return;
+    }
+
+    public List<String> getList() {
+        List<String> list = new LinkedList<>();
+        getList("",list, origin);
+        return list;
+    }
+    private List<String> getList(String s, List<String> list, Node root) {
+        s += root.val;
+        if(root == origin) s = "";
+        if(root.map.containsKey('\0')) list.add(s);
+        for(char c : root.map.keySet()) {
+            getList(s, list, root.map.get(c));
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.attach("car");
         trie.attach("hello");
         trie.attach("cards");
+        trie.attach("hey");
+        trie.attach("hello world");
+        trie.attach("bye");
         System.out.println(trie.search("car"));
         System.out.println(trie.search("cardiac"));
+        trie.print();
+        List<String> list = trie.getList();
+        System.out.println(list);
     }
 }
